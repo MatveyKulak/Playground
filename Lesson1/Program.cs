93c422fc07559d9fcb7070fdcb47bc2d2d321722
Matvey <Matvey@LENOVO1>
2025-02-20 18:28:19 +0300

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        string message;
+
+        if (a > b)
+        {
+            message = "a > b";
+        }
+        else if (a < b)
+        {
+            message = "a < b";
+        }
+        else
+        {
+            message = "a = b";
+        }
+
+        Console.WriteLine(message);
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,36 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        //int a = 22;
+        //int b = 9;
+        //var sum = a + b;
+        //var dif = a - b;
+        //var mul = a * b;
+        //var div = a / b;
+        //Console.WriteLine("Summa: " + sum);
+        //Console.WriteLine("Raznost: " + dif);
+        //Console.WriteLine("Umnozenie: " + mul);
+        //Console.WriteLine("Delenie: " + div);
+
+        //var name = Console.ReadLine();
+
+        //Console.WriteLine("Hello, " + name);
+
+        //string str1 = "Hello";
+        //string str2 = str1;
+        //str1 = str1 + "World";
+
+        //System.Console.WriteLine(str1);
+
+        string name = Console.ReadLine();
+        if (name == "Matvey")
+        {
+            Console.WriteLine("Hello " + name);
+        }
+        else
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
