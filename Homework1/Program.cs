c4ca19d987bb836fb07c851ec5e2f7c4c50a798e
Matvey <Matvey@LENOVO1>
2025-02-22 22:40:46 +0300

    Homework(first task)

---
+++ Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Игра КНБ, выберите камень, ножницы или бумагу");
+        Console.Write("Первый игрок, ваш выбор: ");
+        string player1 = Console.ReadLine();
+        Console.Write("Второй игрок, ваш выбор: ");
+        string player2 = Console.ReadLine();
+
+        if (player1 == player2)
+        {
+            Console.WriteLine("Ничья!");
+        }
+        else if ((player1 == "камень" && player2 == "ножницы") || 
+                 (player1 == "бумага" && player2 == "камень") ||
+                 (player1 == "ножницы" && player2 == "бумага"))
+        {
+            Console.WriteLine("Игрок 1 выиграл!");
+        }
+        else
+        {
+            Console.WriteLine("Игрок 2 выиграл!");
+        }
+    }
+}
