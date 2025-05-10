8bb8990a275fcb564312300a57497756626ae46d
Matvey <Matvey@LENOVO1>
2025-05-10 22:57:58 +0300

    + ?? about reflection

--- Lesson13/PlainClass.cs
+++ Lesson13/PlainClass.cs
@@ -1,6 +1,4 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace Lesson13
+﻿namespace Lesson13
 {
     class PlainClass
     {
---
+++ Reflection/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reflection
+{
+    public class Contact
+    {
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public Contact(string email, int age)
+        {
+            Email = email;
+            Age = age;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Email: {Email}, Возраст: {Age}");
+        }
+    }
+}
