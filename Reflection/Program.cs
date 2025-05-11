0204b626eb6781bd003570130902e6b3617987b5
Matvey <Matvey@LENOVO1>
2025-05-11 14:47:07 +0300

    + some code

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Reflection
+﻿using System.Reflection;
+
+namespace Reflection
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            Type type = Type.GetType("Reflection.Contact");
+            if (type == null)
+            {
+                Console.WriteLine("Класс не найден.");
+                return;
+            }
+
+            Console.WriteLine($"Имя класса: {type.Name}");
+
+            Console.WriteLine("Свойства:");
+            foreach (var property in type.GetProperties())
+            {
+
+            }
+
+            Console.WriteLine("Методы:");
+            foreach (var method in type.GetMethods())
+            {
+                
+            }
+
+            Console.WriteLine("Поля:");
+            foreach (var field in type.GetFields())
+            {
+
+            }
         }
     }
 }
